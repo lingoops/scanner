9227150553243875292af03527696f905655db49
易安 <924190096@qq.com>
2023-03-21 16:06:47 +0800

    demo commit1

---
+++ src/main/java/com/ll/demo.java
@@ -0,0 +1,11 @@
+package com.ll;
+
+import java.util.Scanner;
+
+public class demo {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("请您输入一个数");
+        String s = scanner.nextLine();
+    }
+}
